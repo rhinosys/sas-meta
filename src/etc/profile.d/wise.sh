e170118cfe27296106c3d9a5128d932013dc36bc
root <root@xspl11s949w.cm.cluster>
2020-02-11 14:50:51 +0100

    new version rstudio: Bug latency of console

--- src/etc/profile.d/wise.sh
+++ src/etc/profile.d/wise.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 . /R/pss/lsf/conf/profile.lsf
 . /R/pss/lsf/conf/lsf.conf
+. /opt/pac/lsfsuite/ext/profile.platform
 
-export PATH=$PATH:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.232.b09-0.el7_7.x86_64/bin:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.232.b09-0.el7_7.x86_64/jre/bin
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.232.b09-0.el7_7.x86_64/jre
-export JAVAC=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.232.b09-0.el7_7.x86_64/bin/javac
-export JAVAH=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.232.b09-0.el7_7.x86_64/bin/javah
-export JAR=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.232.b09-0.el7_7.x86_64/bin/jar
+export PATH=$PATH:/usr/lib/jvm/java/bin:/usr/lib/jvm/jre-openjdk/bin
+export JAVA_HOME=/usr/lib/jvm/jre-openjdk
+export JAVAC=/usr/lib/jvm/java/bin/javac
+export JAVAH=/usr/lib/jvm/java/bin/javah
+export JAR=/usr/lib/jvm/java/bin/jar
 export PATH=$PATH:/cm/local/apps/curl/bin
 export LIB_CURL=/cm/local/apps/curl/include/curl
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/curl/lib
@@ -16,8 +17,9 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/microsoft/ropen/3.4.2/lib64/R/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/microsoft/ropen/3.4.3/lib64/R/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/microsoft/ropen/3.5.1/lib64/R/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/microsoft/ropen/3.5.3/lib64/R/lib/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.141-1.b16.el7_3.x86_64/jre/lib/amd64/server
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.141-2.6.10.1.el7_3.x86_64/jre/lib/amd64/server
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.232.b09-0.el7_7.x86_64/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/jre-openjdk/lib/amd64/server
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/jre-openjdk/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/oracle/12.2/client64/lib
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/cm/local/apps/curl/lib/pkgconfig:/usr/lib64/pkgconfig/
+
